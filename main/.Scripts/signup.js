48d69271b8fc629dac3bd95b8e941ca7ac441797
sakitibi <sakitibi10nin@icloud.com>
2025-10-19 15:54:35 +0900

    Merge remote changes

--- main/.Scripts/signup.js
+++ main/.Scripts/signup.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("signup");
 button.addEventListener('click', function(){
-    window.electronAPI.OpenURL("https://asakura-wiki.vercel.app/login/signup");
+    window.electronAPI.OpenURL("https://asakura-wiki.vercel.app/login/13nin/signup");
     window.location.replace("./.login.html");
 });
