6753060d3476dacfa64c7975abe8ffce79bd75ef
sakitibi <sakitibi10nin@icloud.com>
2025-12-12 15:28:21 +0900

    Merge remote changes

--- main/.Scripts/module/supabase.js
+++ main/.Scripts/module/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.6";
 
 function xorEncryptDecrypt(input, key) {
     let output = '';
